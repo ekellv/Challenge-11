e376f4c00a65acf31f98357065a1efdec69d5496
Erin V <erinkvoelker@gmail.com>
2022-08-07 13:29:36 -0400

    debugging html route errors

--- routes/HTMLroutes.js
+++ routes/HTMLroutes.js
@@ -1,16 +1,16 @@
 // dependencies
 const express = require('express');
-const router = express.Router();
 const path = require('path');
 
 // route to get notes.html
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
 
-// wildcard route
-router.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+module.exports = (app) => {
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
 
-module.exports = router;
+    // wildcard route
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    })
+};
