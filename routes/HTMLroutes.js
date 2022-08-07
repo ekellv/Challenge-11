3baa54d1969cb16c1fe4df8f896fa6ecf5b48992
Erin V <erinkvoelker@gmail.com>
2022-08-07 12:49:10 -0400

    adding initial HTML routing

--- routes/HTMLroutes.js
+++ routes/HTMLroutes.js
@@ -1,11 +1,14 @@
+// dependencies
 const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+// route to get notes
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+// wildcard route
 router.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
