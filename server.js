768e9f74153c050265f308584d54355130f19fa1
Erin V <erinkvoelker@gmail.com>
2022-08-11 18:05:34 -0400

    adding comments

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // route files 
-require('./routes/APIroutes')(app);
+require('./routes/APIroutes')(app); // node modules returns a weird error when doing app.use with the routes, stack overflow recommended linking the route without app.use... the error still occurs but the app is functional linking this way
 require('./routes/HTMLroutes')(app);
 
 // // app to use routing files 
